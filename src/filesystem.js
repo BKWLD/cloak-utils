42596c92ea454a18378ff8255ddf5e70f14ff145
Robert Reinhard <robert.reinhard@bukwild.com>
2022-04-16 07:00:54 -0700

    Adding getFilenames util

--- src/filesystem.js
+++ src/filesystem.js
@@ -2,7 +2,7 @@
  * Utils for interacting with the filsystem in @cloak-app/boilerplate
  */
 import { join } from 'path'
-import { existsSync } from 'fs'
+import { existsSync, readdirSync, lstatSync } from 'fs'
 
 // Check if the src directory has a path.  Use like:
 // srcHasFile("store/index.js")
@@ -10,5 +10,13 @@ export function srcHasPath(options, paths) {
 	if (!Array.isArray(paths)) paths = [ paths ]
 	return paths.find(path => {
 		return existsSync(join(options.srcDir, path))
+	})
+}
+
+// Get all the filenames in a directory
+// https://stackoverflow.com/a/69929005/59160
+export function getFilenames(path) {
+	return readdirSync(path).filter(file => {
+		return lstatSync(`${path}/${file}`).isFile()
 	})
 }
